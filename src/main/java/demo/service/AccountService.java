9f240baf0cd1247b2aff7c0c1cd7910773a9eabb
mello-moose <42898454+mello-moose@users.noreply.github.com>
2020-11-11 13:50:29 -0600

    Update AccountService.java

--- src/main/java/demo/service/AccountService.java
+++ src/main/java/demo/service/AccountService.java
@@ -1,5 +1,7 @@
 package demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.github.javafaker.Faker;
 import demo.model.Account;
 import org.springframework.data.domain.Page;
@@ -12,27 +14,32 @@ import javax.transaction.Transactional;
 
 @Service
 public class AccountService {
+    Logger logger = LoggerFactory.getLogger(AccountService.class);
     private static String INSTANCE_NAME = new Faker().funnyName().name();
     @Resource
     AccountRepository accountRepository;
     @Transactional
     public Page<Account> get(Pageable pageable) {
+        logger.info(INSTANCE_NAME + " received a GET request.");
         Page<Account> accountPage = accountRepository.findAll(pageable);
         return accountPage;
     }
     @Transactional
     public Account create(Account account) {
+        logger.info(INSTANCE_NAME + " received a POST request.");
         account.setInstanceName(INSTANCE_NAME);
         return accountRepository.save(account);
     }
     @Transactional
     public Account update(Long id, Account account) {
+        logger.info(INSTANCE_NAME + " received a PUT request.");
         account.setId(id);
         account.setInstanceName(INSTANCE_NAME);
         return accountRepository.save(account);
     }
     @Transactional
     public void delete(Long id) {
+        logger.info(INSTANCE_NAME + " received a DELETE request.");
         accountRepository.deleteById(id);
     }
 }
